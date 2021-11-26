40ba6cb24e92e282bfd9ca3f28d08effe2992957
Alex Antonides <alex.m.antonides@gmail.com>
2021-11-26 23:15:57 +0100

    Update start_elasticsearch_server.sh

--- start_elasticsearch_server.sh
+++ start_elasticsearch_server.sh
@@ -2,8 +2,3 @@
 
 ./assets/elasticsearch-7.9.2/bin/elasticsearch -d
 echo "Elasticsearch is started!"
-
-#!/bin/sh
-./assets/elasticsearch-7.9.2/bin/elasticsearch -d
-echo "Elasticsearch is started!" 
->>>>>>> 827d2532075ce6efd2d3a2b72891a641f99db6f5
