07e225da8899335e4ec62330c020008ca08b46e2
tom Corten <tomcorten@hotmail.com>
2021-11-26 20:21:52 +0100

    last coding done, still have to clean a bit.. please check if function works

---
+++ main_Tom.py
@@ -0,0 +1,113 @@
+# Dependencies
+import gzip
+import threading
+
+# Modules
+from clean import clean
+from ner import get_entities_nltk, get_entities_spacy, get_entities_stanza
+from wikidata_Tom import get_amount_objects, elastic_search, get_random_entities, get_predicates_overlap, check_candidate
+
+KEYNAME = "WARC-TREC-ID"
+
+PERS = get_random_entities('P31' ,'Q5') # get 20 random instances of Q5: human
+LOC = get_random_entities('P31','Q486972') # get 20 random instances of Q486972: human settlement
+ORG = get_random_entities('P31','Q6881511') # get 20 random instances of Q6881511: enterprise
+
+pers_overlap = get_predicates_overlap(PERS)
+org_overlap = get_predicates_overlap(ORG)
+loc_overlap = get_predicates_overlap(LOC)
+
+overlap_dict = {'PERSON' : pers_overlap, 'ORG' : org_overlap, 'GPE' : loc_overlap}
+
+
+def find_labels(payload):
+    result = []
+    if payload == '':
+        return result
+
+    key = None
+    for line in payload.splitlines():
+        if line.startswith(KEYNAME):
+            key = line.split(': ')[1]
+            break
+
+
+    # Get the cleaned text from the payload
+    cleaned = clean(payload)
+
+    if (cleaned!=''):
+
+        # Loop through each named entitiy chunk
+        for chunk in get_entities_spacy(cleaned):
+            # If the chunk is empty continue
+            if chunk == None:
+                continue
+            
+            QUERY = chunk[1]
+            po_dict = {}
+            A = {}
+            try:
+                for entity, labels in elastic_search(QUERY).items():
+                    if chunk[0] in overlap_dict.keys():           
+                        entity_page = check_candidate(chunk, entity ,overlap_dict)
+                        if entity_page:
+                            result.append([key, labels, entity])
+                            print(key, labels, entity)
+                            continue
+                    for candidate_pos in get_amount_objects(entity):
+                        po_dict[entity] = candidate_pos
+                if po_dict:
+                    max_key = max(po_dict, key=po_dict.get)
+                    if (max_key):
+                        result.append([key, QUERY, max_key])
+
+            # Catch connection errors from elastic search
+            except ConnectionError as e:
+                print("Error connecting to elastic search")
+                raise e
+            except Exception as e:
+                print(key, e)
+                continue
+    return result
+
+
+def split_records(stream):
+    payload = ''
+    for line in stream:
+        if line.strip() == "WARC/1.0":
+            yield payload
+            payload = ''
+        else:
+            payload += line
+    yield payload
+
+if __name__ == '__main__':
+    import sys
+    try:
+        _, INPUT = sys.argv
+    except Exception as e:
+        print('Usage: python starter-code.py INPUT')
+        sys.exit(0)
+
+    filename = "sample_predictions.tsv"
+    open(filename, 'w').close()
+
+    threads = []
+    with gzip.open(INPUT, 'rt', errors='ignore') as fo:
+        for record in split_records(fo):
+            def find():
+                results = []
+                for result in find_labels(record):
+                    key = result[0]
+                    label = result[1]
+                    wikidata_id = result[2]
+
+                    #print(key + '\t' + label + '\t' + wikidata_id)
+                    results.append([key, label, wikidata_id])
+                    
+                file = open(filename, "a")  
+                file.write("".join([result[0] + '\t' + result[1] + '\t' + result[2] + '\n' for result in results]))
+                file.close()
+
+            threads.append(threading.Thread(target=find))
+            threads[len(threads) - 1].start()
---
+++ wikidata_Tom.py
@@ -0,0 +1,119 @@
+import json
+
+import trident
+KBPATH='assets/wikidata-20200203-truthy-uri-tridentdb'
+
+from elasticsearch import Elasticsearch
+
+import threading 
+
+lock = threading.Lock()
+
+class TridentHandler:
+    def __init__(self, db):
+        self._terms = {}
+        self._db = db
+
+    def lookup(self, term):
+        if term not in self._terms:
+            self._terms[term] = self._db.lookup_id(term)
+        return self._terms[term]
+    
+    def o_aggr_froms(self, term):
+        return self._db.o_aggr_froms(self.lookup(term))
+
+db = trident.Db(KBPATH)
+handler = TridentHandler(db)
+
+
+def get_amount_objects(entity):
+    lock.acquire()
+    
+    object_from_subject = handler.o_aggr_froms(entity)
+    object_from_subject_text = [handler._db.lookup_str(i) for i in object_from_subject]
+    yield (len(object_from_subject_text))
+    
+    lock.release()
+
+def check_candidate(named_entity, entity_page ,overlap_dict):
+
+    entity_predicates = get_predicates_from_subject(entity_page)
+    if (overlap_dict[named_entity[0]]).issubset(set(entity_predicates)):
+        return entity_page
+   
+
+def elastic_search(query, n=20):
+    e = Elasticsearch()
+    p = { "query" : { "query_string" : { "query" : query }}}
+    response = e.search(index="wikidata_en", body=json.dumps(p), size=n, request_timeout = 30)
+    id_labels = {}
+    if response:
+        for hit in response['hits']['hits']:
+            source = hit['_source']
+            if ('schema_name' in source):
+                label = source['schema_name']
+            elif ('schema_description' in source):
+                label = source['schema_description']
+            else:
+                continue
+            id = hit['_id']
+            id_labels.setdefault(id, set()).add(label)
+    return id_labels
+
+
+def get_predicates_from_subject(subject):
+
+    entitie_pos = db.po(db.lookup_id(subject))
+    predicates = ([po[0] for po in entitie_pos])
+
+    return predicates
+
+
+def get_predicates_overlap(entities):
+
+    #define list which holds predicates for each entity
+    p_list = []
+    #get all predicates
+    for entity in entities:
+        predicates = get_predicates_from_subject(entity)
+        p_list.append(predicates)
+    overlap = (set.intersection(*map(set, p_list)))
+
+    return (overlap)
+
+
+def ne_based_model(candidate, random_overlap):
+
+    #get po's from candidate
+    id_of_test = db.lookup_id(candidate)
+    contents_of_subject = db.po(id_of_test)
+    #check whether predicates of the overlap of five persons is in candidate predicates
+    predicates_of_candidate = [po[0] for po in contents_of_subject]
+    if random_overlap.issubset(set(predicates_of_candidate)):
+        print('CHECK')
+    else:
+        print('NO')
+
+
+def get_random_entities(predicate,label):
+
+    query="""PREFIX wde: <http://www.wikidata.org/entity/> \
+        PREFIX wdp: <http://www.wikidata.org/prop/direct/> \
+        PREFIX wdpn: <http://www.wikidata.org/prop/direct-normalized/> \
+        select ?s where { ?s wdp:""" + predicate + """ wde:""" + label + """ . } 
+        OFFSET 210 #random number variable
+        LIMIT 10
+    """
+
+    #get query of n random entities
+    persons = db.sparql(query)
+    #results = db.sparql(query)
+    json_results = json.loads(persons)
+    variables = json_results["head"]["vars"]
+
+    entity_list = []
+    results = json_results["results"]
+    for b in results["bindings"]:
+        for var in variables:
+            entity_list.append("<"+ b[var]["value"] + ">")
+    return entity_list
